50915a718da10b9da2a30d5a829ab48b47d6cff2
benny-png <mazikuben2@gmail.com>
2024-08-09 00:51:19 +0300

    Initial commit

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ import TypesenseInstantSearchAdapter from "typesense-instantsearch-adapter";
 
 const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
   server: {
-    apiKey: "xyz", // Be sure to use an API key that only allows searches, in production
+    apiKey: "zRPkzYm50IlnE0YoV3c9vmkeJhGsgIuE",
     nodes: [
       {
-        host: "localhost",
-        port: "8108",
-        protocol: "http",
+        host: "x3zi0v6cr9kg1u7jp-1.a1.typesense.net",
+        port: "443",
+        protocol: "https",
       },
     ],
   },
@@ -18,14 +18,16 @@ const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
   //  queryBy is required.
   //  filterBy is managed and overridden by InstantSearch.js. To set it, you want to use one of the filter widgets like refinementList or use the `configure` widget.
   additionalSearchParameters: {
-    queryBy: "title,authors",
+    queryBy: "title,authors,name",  // Include 'name' in the query for full author names
+    queryByWeights: "1,2,3",  // Prioritize 'name' over 'authors'
   },
 });
+
 const { searchClient } = typesenseInstantsearchAdapter;
 
 const search = instantsearch({
   searchClient,
-  indexName: "books",
+  indexName: "research_papers",
 });
 
 search.addWidgets([
@@ -41,15 +43,23 @@ search.addWidgets([
       item(item) {
         return `
         <div>
-          <img src="${item.image_url}" alt="${item.name}" height="100" />
           <div class="hit-name">
             ${item._highlightResult.title.value}
           </div>
           <div class="hit-authors">
           ${item._highlightResult.authors.map((a) => a.value).join(", ")}
           </div>
-          <div class="hit-publication-year">${item.publication_year}</div>
-          <div class="hit-rating">${item.average_rating}/5 rating</div>
+          <div class="hit-publication-year">${item.year}</div>
+          <div class="hit-citations">${item.citations} citations</div>
+          <div class="hit-h-index">${item.h_index} H-index</div>
+          <div class="hit-i10-index">${item.i10_index} I10-index</div>
+          <div class="hit-title-cites">${item.title_cites} title cites</div>
+          <div class="hit-journal">${item.journal}</div>
+          <div class="hit-volume">${item.volume}</div>
+          <div class="hit-pages">${item.pages}</div>
+          <div class="hit-booktitle">${item.booktitle}</div>
+          <div class="hit-organization">${item.organization}</div>
+          <div class="hit-link"><a href="${item.link}">View Paper</a></div>
         </div>
       `;
       },
